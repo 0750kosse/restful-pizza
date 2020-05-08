0e2007f3bc6363865b8835208b844f2fcd0b8145
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2020-05-08 18:41:55 +0100

    Fetches & displays data from API - selfnote: split prods into categories?

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const paths = require('./paths')
+const paths = require('./paths');
+const fetch = require('node-fetch');
 
-function getHome(req, res, next) {
-  res.render('home');
+async function fetchData() {
+  let response = await fetch('https://manolopizza.herokuapp.com/api/menu');
+  let data = await response.json();
+  let productArray = await data.product;
+  return productArray
 }
 
-function getMenu(req, res, next) {
-  res.render('menu');
+function getHome(req, res, next) {
+  res.render('home')
+}
+
+async function getMenu(req, res, next) {
+  let productList = await fetchData();
+  res.render('menu', { item: productList });
 }
 
 function getOrder(req, res, next) {
@@ -24,3 +33,4 @@ router.get(paths.order, getOrder);
 router.get(paths.contact, getContact)
 
 module.exports = router;
+
